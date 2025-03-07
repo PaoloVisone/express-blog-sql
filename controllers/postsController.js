c97b109dda985e15c2831a79dcd0b9f4d760a6f4
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-07 23:05:02 +0100

    [feat] add calback function show

--- controllers/postsController.js
+++ controllers/postsController.js
@@ -17,27 +17,18 @@ function index(req, res) {
 }
 
 function show(req, res) {
-    // copiamo la logica della show
+    // recuperiamo l'id dall' URL
+    const id = req.params.id
 
-    // recupero l'id dall' URL e trasformiamolo in numero
-    const id = parseInt(req.params.id)
-
-    // cerco il post tramite id
-    const post = posts.find(post => post.id === id);
-
-    // Faccio il controllo
-    if (!post) {
-        //Imposto lo status 404
-        res.status(404)
-
-        return res.json({
-            error: "Not Found",
-            message: "Post non trovato"
-        })
-    }
-
-    // Restituisci JSON
-    res.json(post);
+    // Tramite id richiamo la query
+    const sql = 'SELECT * FROM posts WHERE id = ?';
+    // Chiamata al database tramite mysql2
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ error: 'Database query failed' });
+        if (results.length === 0) return res.status(404).json({ error: 'Post not found' });
+        // Traduce la risposta in un array con un oggetto
+        res.json(results[0]);
+    });
 }
 
 function store(req, res) {
